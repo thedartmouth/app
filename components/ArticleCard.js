eb1038a9b4af0808d0d0e7146d95fe515ac41bbc
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-23 01:10:26 -0400

    further built article card

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {
+	Alert,
+	Button,
+	Image,
+	Platform,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
 
 export default function ArticleCard(props) {
   return (
     <TouchableOpacity>
       <View style={styles.articleContent}>
         <View style={styles.articleInfo}>
-          <Text style={styles.articleCategory}>{props.article.category}</Text>
-          <Text style={styles.articleTitle}>{props.article.title}</Text>
-          <Text style={styles.articleAuthor}>by {props.article.author}</Text>
+          <Text style={styles.articleCategory}>{props.article.category.name}</Text>
+          <Text style={styles.articleTitle}>{props.article.headline}</Text>
+          <Text style={styles.articleAuthor}>by {props.article.authors[0].name}</Text>
         </View>
         <View style={styles.imageBox}>
           <Image
-            source={require(props.article.imageURL)}
+            source={require('../assets/images/article1.jpg')}
             style={styles.articleImage}
           />
         </View>
-        <Text>{props.article.articleTextBrief}</Text>
+        <Text>{props.article.content}</Text>
       </View>
     </TouchableOpacity>
   );
