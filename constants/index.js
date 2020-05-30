b3c40eb3b82211b9f3aad58e0905e33e14a8156f
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-30 01:52:22 -0400

    adding URL constants for CMS

--- constants/index.js
+++ constants/index.js
@@ -2,4 +2,10 @@ import Colors from './Colors';
 import Layout from './Layout';
 import Typography from './Typography';
 
-export { Colors, Layout, Typography }
+const CMS_URL = 'https://www.thedartmouth.com';
+
+export const CMSImageUrl = (imageUUID, extension) => {
+  return `https://snworksceo.imgix.net/drt/${imageUUID}.sized-1000x1000.${extension}`;
+}
+
+export { Colors, Layout, Typography, CMS_URL }
