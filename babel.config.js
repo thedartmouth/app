9891d4843196808a64e653cdd61abdcd2df43923
Emma Johnson <emmajohnson@Emmas-MacBook-Pro-4.local>
2022-09-09 19:18:18 -0400

    repairing bug

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
 	api.cache(true)
 	return {
 		presets: ['babel-preset-expo'],
-		plugins: ['@babel/plugin-syntax-optional-chaining'],
+		plugins: ['@babel/plugin-syntax-optional-chaining', 'react-native-reanimated/plugin'],
 	}
 }
